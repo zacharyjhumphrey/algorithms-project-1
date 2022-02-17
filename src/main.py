f290859f0fa822005911c7db4e765c86aa4d54e1
dleslie330 <dleslie@cub.uca.edu>
2022-02-16 21:09:26 -0600

    complete encrypt and decrypt methods
    I completed the encryption and decryption methods, leaving all the important stuff about this algorithm left to complete :)

--- src/main.py
+++ src/main.py
@@ -130,10 +130,20 @@ def generate_private_key(e, phi_N):
     return 0
 
 def encrypt_message(public_key, message_to_encrypt):
-    return 0
+    encrypted = []
+
+    for i in range(len(message_to_encrypt)):
+       encrypted[i] = pow(convert_character_to_number(message_to_encrypt[i]), public_key[0], public_key[1])
+    
+    return encrypted
 
 def decrypt_message(private_key, message_to_decrypt):
-    return 0
+    encrypted = []
+
+    for i in range(len(message_to_decrypt)):
+        encrypted[i] = convert_number_to_character(pow(message_to_decrypt[i], private_key[0], private_key[1]))
+
+    return encrypted
 
 def generate_digital_signature(msg, private_key):
     return 0
