f24523042d98113a3675adde8f7575b5f6e25066
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2022-02-17 09:18:58 -0600

    sketching out test suite

--- tests/unit/example_test.py
+++ tests/unit/example_test.py
@@ -1,13 +1,48 @@
-from src import *
+# from src import Example
+
+def test_convert_character_to_number():
+    pass
+    # assert __convert_character_to_number('a') == 1
+    # assert __convert_character
+
+def test_convert_character_to_number():
+    pass
+
+def test_generate_prime_number():
+    pass
+
+def test_generate_public_key():
+    pass
 
 
-def test_testing():
-    assert True
+def test_generate_private_key(e, phi_N):
+    return 0
 
 
-# def test_passing():
-#     assert Example().value == 7
+def test_encrypt_message(public_key, message_to_encrypt):
+    pass
 
 
-# def test_failing():
+def test_decrypt_message(private_key, message_to_decrypt):
+    pass
+
+
+def test_generate_digital_signature(msg, private_key):
+    pass
+
+
+def test_authenticate_signature(msg, public_key, private_key):
+    pass
+
+
+def test_get_count_coprime_number_count(prime_1, prime_2):
+    pass
+
+
+def test_is_coprime(number_to_check_if_coprime, N):
+    pass
+
+
+
+# def test_test_failing():
 #     assert Example().value < 4
