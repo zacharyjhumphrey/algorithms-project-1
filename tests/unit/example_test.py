3d15f90d9accec5f18439b1147baece1fc7c6679
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2022-02-17 18:57:54 -0600

    creating gcd test

--- tests/unit/example_test.py
+++ tests/unit/example_test.py
@@ -1,15 +1,15 @@
-import pytest
 from backend import *
+import random as rnd
+import pytest
+
+# def test_convert_character_to_number():
+#     pass
+#     # assert __convert_character_to_number('a') == 1
+#     # assert __convert_character
 
 
-def test_convert_character_to_number():
-    pass
-    # assert __convert_character_to_number('a') == 1
-    # assert __convert_character
-
-
-def test_convert_character_to_number():
-    pass
+# def test_convert_character_to_number():
+#     pass
 
 
 def test_generate_prime_number():
@@ -23,36 +23,58 @@ def test_generate_prime_number():
                     "Test generate prime number has returned a prime number")
 
 
-def test_generate_public_key():
-    pass
+# def test_generate_public_key():
+#     pass
 
 
-def test_generate_private_key(e, phi_N):
-    return 0
+# def test_generate_private_key(e, phi_N):
+#     return 0
 
 
-def test_encrypt_message(public_key, message_to_encrypt):
-    pass
+# def test_encrypt_message(public_key, message_to_encrypt):
+#     pass
 
 
-def test_decrypt_message(private_key, message_to_decrypt):
-    pass
+# def test_decrypt_message(private_key, message_to_decrypt):
+#     pass
 
 
-def test_generate_digital_signature(msg, private_key):
-    pass
+# def test_generate_digital_signature(msg, private_key):
+#     pass
 
 
-def test_authenticate_signature(msg, public_key, private_key):
-    pass
+# def test_authenticate_signature(msg, public_key, private_key):
+#     pass
 
 
-def test_get_count_coprime_number_count(prime_1, prime_2):
-    pass
+# def test_get_count_coprime_number_count(prime_1, prime_2):
+#     pass
 
 
-def test_is_coprime(number_to_check_if_coprime, N):
-    pass
+# def test_is_coprime(number_to_check_if_coprime, N):
+#     pass
+
+
+def test_gcd():
+    for i in range(10):
+        x = rnd.randint(2, 70)
+        y = x * rnd.randint(2, 70)
+        assert gcd(x, y) == x
+
+    for i in range(10):
+        x = rnd.randint(2, 70)
+        y = 2 * rnd.randint(2, 70)
+        z = 3 * rnd.randint(2, 70)
+        assert gcd(z, y) == x
+
+
+def test_gcd_with_prime_numbers():
+    for i in range(10):
+        x = generate_prime_number(80, 300)
+        y = 0
+        while y != x:
+            y = rnd.randint(80, 300)
+        assert gcd(x, y) == 1
 
 
 # def test_test_failing():
